12a59ba7384b879e12ef8488351db312db24b92e
Carlos Buitrago <letscatchupagain@gmail.com>
2024-08-13 12:32:44 +0100

    add burger menu with animation for smaller screens

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+// TITLE ANIMATION
 $(document).ready(() => {
     $('.logo').textillate({
         in: {
             effect: 'fadeInDownBig',
             delay: 20,
         },
+    });
+});
+
+// TOGGLE DROPDOWN MENU IN MOBILE
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerMenu = document.querySelector('.burger__menu input');
+    const navbar = document.querySelector('.navbar');
+
+    burgerMenu.addEventListener('change', () => {
+        if (burgerMenu.checked) {
+            navbar.classList.add('show');
+        } else {
+            navbar.classList.remove('show');
+        }
     });
 });
