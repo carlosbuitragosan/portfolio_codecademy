70c906e6333a2b90c46608b79e8b4517e3b8c4dc
Carlos Buitrago <letscatchupagain@gmail.com>
2024-08-24 20:19:38 +0100

    add ripple effect to hero image.

--- js/modules/heroAnimation.js
+++ js/modules/heroAnimation.js
@@ -1,4 +1,6 @@
 export function heroAnimation() {
+    const $hero = $('.hero');
+
     $('.hero__text').each(function () {
         $(this).textillate({
             initialDelay: 1000,
@@ -8,15 +10,26 @@ export function heroAnimation() {
             },
         });
     });
-}
 
-export function loadHeroImage() {
-    console.log('loading image');
-    const $hero = $('.hero');
-    const bigImage = new Image();
-    bigImage.src = '../imgs/london_desktop.webp';
+    // RIPPLES ANIMATION
+    $hero.ripples({
+        resolution: 128,
+        dropRadius: 20,
+        perturbance: 0.004,
+        interactive: false,
+    });
 
-    bigImage.onload = function () {
-        $hero.addClass('loaded');
-    };
+    const $heroOffset = $hero.offset();
+    const heroWidth = $hero.width();
+    const heroHeight = $hero.height();
+    const x = $heroOffset.left + heroWidth / 2;
+    const y = $heroOffset.top + heroHeight / 2;
+
+    function triggerRipple(width, height) {
+        $hero.ripples('drop', width, height, 5, 0.2);
+    }
+
+    setTimeout(() => {
+        triggerRipple(x, y);
+    }, 500);
 }
